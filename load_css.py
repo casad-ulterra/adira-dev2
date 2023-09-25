11bcf26c24b1a2b7109bb5882065c33d68dbf493
casad-ulterra <80429374+casad-ulterra@users.noreply.github.com>
2023-09-25 11:17:21 -0500

    Delete load_css.py

--- load_css.py
+++
@@ -1,7 +0,0 @@
-# https://discuss.streamlit.io/t/colored-boxes-around-sections-of-a-sentence/3201/2
-
-import streamlit as st
-
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
